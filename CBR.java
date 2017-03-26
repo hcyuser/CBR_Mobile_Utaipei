b6741818fac949447d278b40b743930eb48b245b
CHIH-YANG HUANG <user@hcy.idv.tw>
2017-03-26 12:25:34 +0800

    Add the data file to inspect whether it is working.

--- CBR.java
+++ CBR.java
@@ -3,6 +3,6 @@ public class CBR {
 	public static void main(String[] args){
 		CBRC Test = new CBRC("TRAVEL.csv");
 		System.out.println(Test.DataArr.size());
-		System.out.println(Test.DataArr.get(Test.CountCBR("Skiing", 589, 4, "Car", 7, "January").get(2).getKey()).CaseName);
+		System.out.println(Test.DataArr.get(Test.CountCBR("Skiing", 589, 100, "Plane", 7, "January").get(2).getKey()).CaseName);
 	}
 }

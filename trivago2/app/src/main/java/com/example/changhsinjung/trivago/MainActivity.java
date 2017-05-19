a24ee30d0481c7d0d6a6ad608ae1b31ab163cfd0
CHIH-YANG HUANG <user@hcy.idv.tw>
2017-05-19 10:48:23 +0800

    Use Bundle to connect the two activity

--- trivago2/app/src/main/java/com/example/changhsinjung/trivago/MainActivity.java
+++ trivago2/app/src/main/java/com/example/changhsinjung/trivago/MainActivity.java
@@ -17,33 +17,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button search = (Button)findViewById(R.id.search);
-        EditText money = (EditText) findViewById(R.id.money);
-        EditText people = (EditText) findViewById(R.id.people);
-        EditText day = (EditText) findViewById(R.id.day);
-        search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(MainActivity.this , page2.class);
-                startActivity(intent);
-            }
-        });
+        final Button search = (Button)findViewById(R.id.search);
+        final EditText money = (EditText) findViewById(R.id.money);
+        final EditText people = (EditText) findViewById(R.id.people);
+        final EditText day = (EditText) findViewById(R.id.day);
 
-        Spinner chooseway = (Spinner)findViewById(R.id.chooseway);
+
+       final  Spinner chooseway = (Spinner)findViewById(R.id.chooseway);
         final String[] lunch = {"Bathing", "Active", "Education", "City", "Recreation", "Wandering","Skiing"};
         ArrayAdapter <String> lunchList = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, lunch);
         chooseway.setAdapter(lunchList);
 
 
-        Spinner choosevehicle = (Spinner)findViewById(R.id.choosevehicle);
+        final Spinner choosevehicle = (Spinner)findViewById(R.id.choosevehicle);
         final String[] car = {"Plane", "Car", "Train"};
         ArrayAdapter <String> carList = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, car);
         choosevehicle.setAdapter(carList);
 
-        Spinner choosemonth = (Spinner)findViewById(R.id.choosemonth);
+        final Spinner choosemonth = (Spinner)findViewById(R.id.choosemonth);
         final String[] month = {"January","Febuary","March","April","May","June","July","August","Septemper","October","November","December"};
         ArrayAdapter <String> monthList = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_spinner_dropdown_item, month);
         choosemonth.setAdapter(monthList);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this , page2.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("money",Integer.valueOf(money.getText().toString()));
+                bundle.putInt("people",Integer.valueOf(people.getText().toString()));
+                bundle.putInt("day",Integer.valueOf(day.getText().toString()));
+                bundle.putString("chooseway",chooseway.getSelectedItem().toString());
+                bundle.putString("choosevehicle",choosevehicle.getSelectedItem().toString());
+                bundle.putString("choosemonth",choosemonth.getSelectedItem().toString());
+                intent.putExtras(bundle);
+                startActivity(intent);
+
+            }
+        });
     }
 }
--- trivago2/app/src/main/java/com/example/changhsinjung/trivago/page2.java
+++ trivago2/app/src/main/java/com/example/changhsinjung/trivago/page2.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.EditText;
 
 public class page2 extends AppCompatActivity {
     @Override
@@ -15,14 +16,20 @@ public class page2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
-
         Button back = (Button)findViewById(R.id.back);
         back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-
+        Bundle bundle =this.getIntent().getExtras();
+        int money = bundle.getInt("money");
+        int people = bundle.getInt("people");
+        int day = bundle.getInt("day");
+        String chooseway = bundle.getString("chooseway");
+        String choosevehicle = bundle.getString("choosevehicle");
+        String choosemonth = bundle.getString("choosemonth");
+        final EditText result = (EditText) findViewById(R.id.result);
 
     }
 
